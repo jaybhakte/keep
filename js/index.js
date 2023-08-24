115d0dbdda6a92287268eb0242acf4601a32b4f9
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2023-08-24 19:07:42 +0530

    delete and search functions

--- js/index.js
+++ js/index.js
@@ -1,27 +1,117 @@
-console.log("Project 1");
+showNotes();
 
-//select add button
 let addBtn = document.getElementById("addBtn");
-//apply click event
+
 addBtn.addEventListener("click", addNotes);
-//addnotes function
-function addNotes() {
-    //title and note element selected
-    let title = document.getElementById("title");
-    let note = document.getElementById("note");
 
-    let html = document.getElementById("notes");
+function addNotes(){
+  //select title box and note box
+  let titleBox = document.getElementById("title");
+  let noteBox = document.getElementById("note"); 
 
-    html.innerHTML += `
-    <div class="col ml-3 mb-3">
-    <div class="card  h-100" style="width: 18rem;">
-    <div class="card-body ">
-      <h5 class="card-title">${title.value}</h5>
-      <p class="card-text">${note.value}</p>
-      <a href="#" class="btn btn-primary">Delete</a>
+  //if it is empty then show alert massage
+  if(titleBox.value=="" || noteBox.value==""){
+    let alert = document.getElementById("alert");
+
+    alert.innerHTML+= `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Carefull!! </strong>Please first add Your notes!!!
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`;
+  }else{ 
+    let notesObj = {
+      title : titleBox.value,
+      note : noteBox.value
+    }
+  
+  //check localstorage notes parameter
+    let notes = localStorage.getItem("notes"); //we get json 
+    // console.log(notes)
+    
+    if(notes==null){
+      arr = []; //if not make empty array
+    }else{
+      arr = JSON.parse(notes) //parse json and convert to js array
+    }
+  
+   arr.push(notesObj) //object push karre into array 
+  localStorage.setItem("notes",JSON.stringify(arr)); //set this array into localstorage as value
+  
+  //empty title and note box
+  titleBox.value =""; 
+  noteBox.value ="";
+  
+  showNotes();
+
+  }
+
+}
+
+function showNotes(){
+  let notes = localStorage.getItem("notes");//string type
+  
+  if(notes == null){
+    arr = [];
+  }else{
+    arr = JSON.parse(notes); //object 
+  }
+  
+  // console.log(arr.length)
+  let html = "";
+
+  arr.forEach(function(ele,index){
+    html+=`<div class="col noteCard">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${ele.title}</h5>
+        <p class="card-text">${ele.note}</p>
+        <a href="#" id="${index}" class="btn btn-primary" onclick="delNotes(this.id)">Delete</a>
+      </div>
     </div>
-  </div>
-  </div>
-  `
+  </div>`;
+
+})
+let cards = document.getElementById("notes");
+  if(arr.length!=0){
+    cards.innerHTML=html;
+  }else{
+    cards.innerHTML= `<h3 class="text-danger">There is no notes ....!!!</h3>`;
+  }
+
+}
+
+function delNotes(index){
+  let notes = localStorage.getItem("notes");
+  if(notes==null){
+    arr=[];
+  }else{
+    arr = JSON.parse(notes);
+  }
+
+  arr.splice(index,1);
+  localStorage.setItem("notes",JSON.stringify(arr))
+
+  showNotes();
+}
+
+let searchBox = document.getElementById("search");
+
+searchBox.oninput = function(){
+ let searchText = searchBox.value.toLowerCase();
+let allCards = document.getElementsByClassName("noteCard"); //array returns of same class
+
+Array.from(allCards).forEach(function(ele){
+  // console.log(ele)
+  
+ let allHeads = ele.getElementsByTagName("h5")[0].innerHTML;
+
+ if(allHeads.includes(searchText)){
+  ele.style.display = "block";
+ }else{
+  ele.style.display = "none";
+ }
+ console.log(allHeads)
+
+})
+ 
 }
 
